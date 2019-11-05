0db49666fcd1917e8cdb37fc56c67a170fc6856d
NatJWalker-Hale <nathanael.walker.hale@gmail.com>
2019-11-05 11:58:24 +0000

    updates

--- scripts/check_aa_and_cds_trans.py
+++ scripts/check_aa_and_cds_trans.py
@@ -44,11 +44,11 @@ def find_orf_all(seq):
     """Given a string representing a coding sequence, find all ORFs for 
     all frames on the + and - strand"""
     all_orfs = []
-    all_orfs.extend(find_orf(seq))
+    all_orfs.extend(find_orf(seq[0:]))
     all_orfs.extend(find_orf(seq[1:]))
     all_orfs.extend(find_orf(seq[2:]))
     revc = Seq.reverse_complement(seq)
-    all_orfs.extend(find_orf(revc))
+    all_orfs.extend(find_orf(revc[0:]))
     all_orfs.extend(find_orf(revc[1:]))
     all_orfs.extend(find_orf(revc[2:]))
     return list(set(all_orfs))
@@ -72,20 +72,43 @@ def parse_fasta(path): # courtesy of Jonathan Chang https://gist.github.com/jonc
             yield name, sequence
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("usage: python "+sys.argv[0]+" cds")
+    if len(sys.argv) != 3:
+        print("usage: python "+sys.argv[0]+" cds aa")
         sys.exit()
 
-    fa_dict = dict([x for x in parse_fasta(sys.argv[1])])
+    """This script extends my ORF finder script to take a cds and corresponding aa translation, calculate all ORFs, and find the CDS that matches the aa for
+    that sequence. It is designed for the case where one might have a mix of cds and transcripts and doesn't want to manually figure out the ORFs."""
+
+    cds_dict = dict([x for x in parse_fasta(sys.argv[1])])
+    aa_dict = dict([x for x in parse_fasta(sys.argv[2])])
+    # verify that files match
+    cds_seq = [x for x in cds_dict.keys()]
+    aa_seq = [x for x in aa_dict.keys()]
+    if set(cds_seq) != set(aa_seq):
+        print("These files do not contain the same set of sequences!")
+        sys.exit()
+    
+    # getting ORFs
     orf_dict = {}
-    for key, value in fa_dict.items():
+    for key, value in cds_dict.items():
         orfs = find_orf_all(value)
         orflist = []
         for orf in orfs:
             orflist.append((orf,Seq.translate(orf)))
         orf_dict[key] = sorted(orflist, reverse=True, key=lambda x: len(x[0]))
-    print(orf_dict)
     
+    # check translations against aa
+    for key, value in orf_dict.items():
+        match = False
+        for f in value:
+            if f[1].rstrip("*") == aa_dict[key].rstrip("*"):
+                match = True
+                print(">"+key)
+                print(f[0])
+        if not match:
+            print(">"+key)
+            print(cds_dict[key])
+            sys.stderr.write("Warning: no match found for "+key+"! Printing original CDS\n")  
 
 
 
